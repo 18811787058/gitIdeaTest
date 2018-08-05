85bde2dfca050f4e0d0c9df5c885d0c9f5472ec7
hexiaolong@innolife.cc <hexiaolong@innolife.cc>
2018-08-05 22:14:13 +0800

    secound upload

--- src/com/inno/TestGit.java
+++ src/com/inno/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         String a = "ss";
         System.out.println("this is test git");
+        System.out.println("this is test brach");
     }
 }
